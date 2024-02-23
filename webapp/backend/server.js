4e1cd290e3decf7e3c957361ea7ffc6eb8370ef0
frankzamma <frank581.fgz@gmail.com>
2024-02-23 16:06:30 +0100

    Aggiunta registrazione utenti

--- webapp/backend/server.js
+++ webapp/backend/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const cors = require("cors"); // Import the CORS middleware
+const User = require("./models/user")
+const bcrypt = require("bcrypt");
+
 require("dotenv").config();
 
 const app = express();
@@ -19,6 +22,49 @@ mongoose
 app.use(cors()); // Use CORS middleware to allow requests from the frontend
 app.use(express.json());
 app.use("/api/auth", authRoutes); // All the routes defined in auth.js will be prefixed with /api/auth
+
+const users = [
+    {"username" : "frank",
+        "password" : "2002",
+        "OrgName" : "Org1"},
+    {"username" : "amigli",
+        "password" : "2002",
+        "OrgName" : "Org2"}]
+
+const username1 = "frank"
+const username2 = "amigli"
+const password1 = "2002"
+const password2 = "2002"
+const OrgName1  ="Org1"
+const OrgName2 = "Org2"
+
+const user1 = bcrypt.genSalt(10)
+    .then(
+    salt =>{
+        return bcrypt.hash(password1, salt)
+    })
+    .then(hash =>{
+        return new User({
+                username: username1,
+                OrgName: OrgName1,
+                password: hash
+            }
+        )
+    });
+
+const user2 = bcrypt.genSalt(10)
+    .then(
+        salt =>{
+            return bcrypt.hash(password2, salt)
+        })
+    .then(hash =>{
+        return new User({
+                username: username2,
+                OrgName: OrgName2,
+                password: hash
+            }
+        )
+    });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
