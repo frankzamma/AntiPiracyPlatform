4bc5c253114c8da12f1ad3d13ed6870f83784ded
frankzamma <frank581.fgz@gmail.com>
2024-02-24 19:26:51 +0100

    Aggiunto frontend form titolari dei diritti

--- webapp/frontend/src/App.js
+++ webapp/frontend/src/App.js
@@ -9,6 +9,7 @@ import Login from "./components/LoginForm"
 import Logout from "./components/Logout";
 import Dashboard from "./components/Dashboard";
 import { AuthProvider } from "./components/AuthContext";
+import FormTitolareDiritti from "./components/Form/FormTitolareDiritti";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/logout" element={<Logout />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/form-aggiungi-richiesta" element={<FormTitolareDiritti />} />
             <Route path="*" element={<Navigate to="/login" replace />} />
           </Routes>
         </AuthProvider>
---
+++ webapp/frontend/src/components/Form/FormTitolareDiritti.js
@@ -0,0 +1,127 @@
+import {useContext, useState} from "react";
+import {AuthContext} from "../AuthContext";
+import {Navigate, useNavigate} from "react-router-dom";
+import axios from "axios";
+import {string} from "prop-types";
+import TitolareDeiServiziNav from "../titolareDeiServiziNav";
+
+function FormTitolareDiritti() {
+    const { token, loading, orgName} = useContext(AuthContext);
+    const [ipAddress, setIpAddress] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState(null)
+    const [categoria, setCategoria] = useState("");
+    const navigate = useNavigate();
+
+    if (loading) {
+        return null;
+    }
+
+    if (!token) {
+        return <Navigate to="/login" replace />;
+    }
+
+    if(orgName != "Org1"){
+        return <h1>Non sei autorizzato ad accedere a questa pagina</h1>
+    }
+
+
+
+    const handleIpAddressChange = (e) =>{
+        setIpAddress(e.target.value);
+    }
+
+    const handleDscriptionChange = (e) =>{
+        setDescription(e.target.value);
+    }
+
+    const handleImagesChange = (e) =>{
+        setImage(e.target.files[0])
+    }
+
+    const handleCategoriaChange = (e) =>{
+        setCategoria(e.target.selected)
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            if (!image) {
+                return;
+            }
+
+            const formData = new FormData()
+
+            formData.append("ipAddress", ipAddress)
+            formData.append("description", description)
+            formData.append("file", image)
+            formData.append("categoria", categoria)
+
+
+            const response = await axios.post("/save-request", formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+            navigate("/dashboard")
+        } catch (error) {
+            console.error("Send Failed", error);
+        }
+    };
+
+    return (
+        <div>
+            <TitolareDeiServiziNav></TitolareDeiServiziNav>
+            <div className="container" style={{backgroundColor: "white"}}>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="ipInput" className="form-label">Ip address</label>
+                        <input className="form-control" type="text" id="ipInput" value={ipAddress}
+                               onChange={handleIpAddressChange} placeholder="IpAddress"/>
+                    </div>
+                    <div>
+                        <label htmlFor="description" className="form-label">Description</label>
+                        <input className="form-control" type="text" id="description" value={description}
+                               onChange={handleDscriptionChange}
+                               placeholder="description"/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="formFile" className="form-label">Immagini</label>
+                        <input className="form-control" type="file" onChange={handleImagesChange}/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="formFile" className="form-label">Categoria</label>
+                        <select className="form-select" onChange={handleCategoriaChange}  aria-label="Default select example">
+                            <option selected>Open this select menu</option>
+                            <option value="0">Basket</option>
+                            <option value="1">Boxing</option>
+                            <option value="2">Fencing</option>
+                            <option value="3">Football</option>
+                            <option value="4">Formula 1</option>
+                            <option value="5">Moto Gp</option>
+                            <option value="6">Swimming</option>
+                            <option value="7">Tennis</option>
+                            <option value="8">Volleyball</option>
+                        </select>
+                    </div>
+                        <br/>
+
+                        <button>Upload</button>
+                </form>
+
+
+                <br/>
+                <br/>
+            </div>
+        </div>
+
+
+);
+
+
+}
+
+export default FormTitolareDiritti;
