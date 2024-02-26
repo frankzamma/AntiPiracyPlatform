b4b76c1ff108290d60c2d72f133bfcbc1815ec6b
frankzamma <frank581.fgz@gmail.com>
2024-02-26 23:55:47 +0100

    Fix calculate hash file

--- webapp/backend/routes/SalvaRichiesta.js
+++ webapp/backend/routes/SalvaRichiesta.js
@@ -29,65 +29,63 @@ router.post("/save-request", verifyTokenRequest, upload.single('file'),
         let description = req.body.description
         let category = req.body.categoria
 
-        const hash = crypto.createHash("sha256");
-        const stream = fs.createReadStream(path);
 
+        const fileData = fs.readFileSync(path);
 
-        //si calcola l'hash del file
-        stream.on("error", error => {
-            console.error(error)
-            return res.status(501).send('Internal error');
-        })
+        // Calculate hash of the file
+        const hash = crypto.createHash('sha256');
+        hash.update(fileData);
+        const fileHash = hash.digest('hex');
 
         hash.setEncoding("hex")
-        let hashFile
-        stream.on('end', () => {
-            hash.end();
 
-            hashFile = hash.read()
-        });
-        stream.pipe(hash);
+        console.log(hash)
+        try {
+            /*const config = {
+                method: 'post',
+                url: 'https://example.com/upload', // Change this URL to your endpoint
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+                data: formData,
+            };
 
-        const request =  {
-            "ID": id,
-            "IpAddress": ipAddress,
-            "Description": description,
-            "Category": category,
-            "HashImage": hashFile,
-            "PathImage": path,
-            "Verified" : true,
-            "Confirmed" :{
-                "Org2" : false
+
+                // Send the POST request
+            const response = await axios(config);
+            console.log(response.data);*/
+
+
+
+
+            const args = id + "," + ipAddress + "," + description +
+                "," + fileHash + "," + path + "," + true + "," + "Org2" + "," + category
+
+            let data = {
+                "channelid": "mychannel",
+                "chaincodeid": "requestManage",
+                "function": "AddRequest",
+                "args": args
             }
-        }
 
 
-        const args =  id + "," + ipAddress + "," + description +
-            "," + hashFile + "," +  path  + "," +  true + "," + "Org2" + "," +  category
-
-        let data = {
-          "channelid": "mychannel",
-           "chaincodeid": "requestManage",
-           "function": "AddRequest",
-            "args": args
-        }
-
-
-
-        try{
-            const response = await axios.post('http://localhost:3003/invoke', data,
-                {            headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            })
-            console.log('Response:', response.data);
-            res.status(200).send( response.data.toString())
+            try {
+                const response = await axios.post('http://localhost:3003/invoke', data,
+                    {
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    })
+                console.log('Response:', response.data);
+                res.status(200).send(response.data.toString())
+            } catch (error) {
+                console.log(error)
+                res.status(501).send('Request error');
+            }
         }catch(error){
             console.log(error)
             res.status(501).send('Request error');
         }
-
-
 
 
         console.log(req.body)
