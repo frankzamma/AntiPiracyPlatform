eabc996f3726aaf151661192b6e127aeb2d5184a
frankzamma <frank581.fgz@gmail.com>
2024-03-01 23:56:47 +0100

    Fix error riferimento

--- webapp/frontend/src/components/OperatoreNav.js
+++ webapp/frontend/src/components/OperatoreNav.js
@@ -5,7 +5,7 @@ const OperatoreNav = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Anti Piracy Platform</a>
+                <a className="navbar-brand" href="/">Anti Piracy Platform</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -14,7 +14,7 @@ const OperatoreNav = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">Home</a>
+                            <a className="nav-link" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
